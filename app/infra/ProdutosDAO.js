20f2fe941f685bcf40fd6a4f5b614e4e324af156
Anderson Abreu do Nascimento <anderson.abreu@prodam.am.gov.br>
2017-04-03 10:34:55 -0400

    Implementação até fim da Aula 04 - Node.js.

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -5,6 +5,9 @@ function ProdutosDAO(connection) {
 ProdutosDAO.prototype.lista = function (callback) {
     this._connection.query('SELECT * FROM produtos', callback);
 };
+ProdutosDAO.prototype.salva = function (produto, callback) {
+    this._connection.query('INSERT INTO produtos SET ?', produto, callback);
+};
 
 module.exports = function () {
     return ProdutosDAO;
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,13 +2,33 @@ module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         var connection = app.infra.connectionFactory();
         var produtos = new app.infra.ProdutosDAO(connection);
-        // consulta
-        produtos.lista(function(err, results) {
-            //res.send(results);
-            console.log("Listando produtos...");
-            res.render('produtos/lista', {lista: results});
+        produtos.lista(function (err, results) {
+            res.format({
+                html: function() {
+                    res.render('produtos/lista', {lista: results});
+                },
+                json: function() {
+                    res.json(results);
+                }
+            });
         });
         connection.end();
+    });
+
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos', function (req, res) {
+        var produto = req.body;
+        console.log(produto);
+        
+        var connection = app.infra.connectionFactory();
+        var produtos = new app.infra.ProdutosDAO(connection);
+        produtos.salva(produto, function (err, results) {
+            console.log(err);
+            res.redirect('/produtos');
+        });
     });
 
     //app.get('/produtos/detalhe', function (req, res) {
@@ -23,12 +43,12 @@ module.exports = function (app) {
     //    connection.end();
     //});
 
-    app.get('/produtos/remove', function(req, res){
+    app.get('/produtos/remove', function (req, res) {
         var connection = app.infra.connectionFactory();
         var produtosBanco = app.infra.produtosBanco(connection);
         var produto = produtosBanco.carrega(id, callback);
-        if(produto) {
-            produtosBanco.remove(connection,produto,callback);
+        if (produto) {
+            produtosBanco.remove(connection, produto, callback);
         }
 
     });
